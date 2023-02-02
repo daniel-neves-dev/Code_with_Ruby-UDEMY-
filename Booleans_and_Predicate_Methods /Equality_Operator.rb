f37000b9964bf208dbcbaa03527dd3f6e613a27e
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-02-02 17:39:57 -0300

    created: Equality file

---
+++ Booleans_and_Predicate_Methods /Equality_Operator.rb
@@ -0,0 +1,23 @@
+#Equality operator compare data
+#Example
+
+puts 10 == 10
+puts 2 == 3
+
+a = 10
+b = 5
+c = 10
+
+puts 
+
+a == b
+c == a
+puts 
+
+puts "hello" == "hello"
+puts "hello" == "bicycle"
+puts "hello" == "Hello"
+puts
+puts "5".to_i == 5
+puts "5" == 5.to_s
+
