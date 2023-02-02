6eb5e9779ead8da25d64edca86f4735197909251
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-02-02 18:33:58 -0300

    created: Predicate methods

---
+++ Booleans_and_Predicate_Methods /Predicate_Methods.rb
@@ -0,0 +1,21 @@
+#object.method?
+#Examples:
+puts
+
+puts 10.odd?
+puts 11.odd?
+
+puts
+
+puts 1.even?
+puts 2.even?
+
+puts
+
+puts 10.positive?
+puts 10.negative?
+
+puts
+
+puts -8.positive?
+puts -8.negative?
