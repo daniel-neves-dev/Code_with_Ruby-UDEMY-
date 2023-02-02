b34e87775dcb79769c4f47162a512f3913062c6c
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-02-02 17:31:22 -0300

    made: intro boolean file

---
+++ Booleans_and_Predicate_Methods /Intro_to_Boleans.rb
@@ -0,0 +1,21 @@
+#The boolean return true or false
+#Example:
+
+puts 5 < 10
+puts 5 > 10
+
+puts 
+
+puts "apple" == "banana"
+
+puts 
+
+puts 5+5 == 12
+puts 5+5 == 10
+
+puts 
+
+handsome = true
+ugly = false
+puts handsome.class
+puts ugly.class
