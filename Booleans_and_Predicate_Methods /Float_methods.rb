4d944fb1466ed314961f38ec9f1c1f72bb3b7b86
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-02-02 19:02:53 -0300

    created: Floatfile

---
+++ Booleans_and_Predicate_Methods /Float_methods.rb
@@ -0,0 +1,29 @@
+#Examples
+
+puts 10.3.floor 
+puts 10.6.floor
+puts 10.5.floor.class
+
+puts 
+
+puts 10.3.ceil
+puts 10.9.ceil
+puts 10.5.ceil.class
+
+puts 
+
+puts 3.14159.round
+puts 3.86.round
+puts 3.86.round.class
+
+puts 
+
+puts 3.14159.round(2)
+puts 3.14859.round(2)
+puts 3.14859.round(2).class
+
+puts 
+
+puts 5.35.abs #absolut
+puts -5.35.abs
+puts -5.35.abs.class
