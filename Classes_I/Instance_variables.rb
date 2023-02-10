4eb4a0f290926b2d9a14123380892a1cb5473865
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-02-10 19:26:04 -0300

    fix: bugfix initialize instance variables

--- Classes_I/Instance_variables.rb
+++ Classes_I/Instance_variables.rb
@@ -1,5 +1,7 @@
 class Gadget
-  @username
-  @password
-  @production_number
+  def initialize
+    @username = "User #{rand(1..100)}" #random numbers - instance variables
+    @password = "topsevret" #fixed variable
+    @production_number = "#{("a".."z").to_a.sample}-#{rand(99..1000)}"
+  end
 end
