f1d6094fab51e53a97d75dde1da79c50d8309ab9
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-02-11 19:27:32 -0300

    fix: bug fix new password

--- Classes_II/Validation_Setter_Method.rb
+++ Classes_II/Validation_Setter_Method.rb
@@ -7,10 +7,20 @@ class Gadget
     @password = password
   end
 
+  def password=(new_password)
+    @password = new_password if validade_password(new_password)
+  end
+
   def validade_password(new_password)
     new_password.is_a?(String) && new_password.length >= 6 && new_password=~/\d/
   end
 end
 
 phone = Gadget.new("user001", "cumputer001")
+puts phone.password
+phone.password = 321
+puts phone.password
+phone.password = "oliveira"
+puts phone.password
+phone.password = "oliveira321"
 puts phone.password
