cdb67ecec4e48af84075e19518ff7ae2b35a8703
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-02-11 20:21:45 -0300

    feat: created user name and password

--- Classes_II/MultipleSetterMethod.rb
+++ Classes_II/MultipleSetterMethod.rb
@@ -1,10 +1,26 @@
 class Gadget
 
+  attr_accessor :username, :password
+  attr_reader :production_number
+
   def initialize(username, password)
     @username = username
     @password = password
     @production_number = automatic_production_number
   end
-  
+
+  def automatic_production_number
+    digits = rand(100..999)
+    alphabet = ("A".."Z").to_a.sample(2).join
+    year = 2023
+    "#{digits}#{alphabet}#{year}"
+  end
+
+  def info
+    "User name: #{self.username} - Password: #{self.password} - Production number: #{self.production_number}"
+  end
 
 end
+
+phone = Gadget.new("Oliveira", "JamesBond963")
+puts phone.info
