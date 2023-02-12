6ddafd605279fcdac1474bfe81fbedf5430334c6
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-02-12 14:48:25 -0300

    feat: install and delete app

---
+++ Classes_II/Structs/App_store.rb
@@ -0,0 +1,15 @@
+module AppStore
+
+  App = Struct.new(:name, :developer, :version)
+
+  APPS = [
+    App.new(:Chat, :Facebook, 2.0),
+    App.new(:Twitter, :twitter, 5.8),
+    App.new(:Weather, :Yahoo, 10.15)
+  ] 
+
+  def self.find_app(name)
+    APPS.find {|app| app.name == name}
+  end
+
+end
--- Classes_II/Structs/Gadget.rb
+++ Classes_II/Structs/Gadget.rb
@@ -1,11 +1,16 @@
+require_relative 'App_store.rb'
+
 class Gadget
 
   attr_accessor :username, :password
+  attr_reader :production_number
+  attr_reader :apps
 
   def initialize(username, password)
     @username = username
     @password = password
     @production_number = generate_prodnumber
+    @apps = []
   end
 
   #Generating production number
@@ -16,13 +21,40 @@ class Gadget
     "#{digits}#{lethers}#{year}"
   end
 
+  def reset(username, password)
+    self.username = username
+    self.password = password
+    self.apps = []
+  end
+
+  #Reseting password
+  def reset_password(new_password)
+    new_password.is_a?(String) && new_password.length >= 6 && new_password =~/\d/
+  end
+  def password=(new_password)
+    @password = new_password if reset_password(new_password)
+  end
+
   #General gadget informations
   def info
     "User name: #{self.username} - Password: #{self.password} - Production number: #{self.production_number}"
   end
-  
+
+  #Install APP
+  def install_app(name)
+    app = AppStore.find_app(name)
+    @apps << app unless @app.include?(app)
+  end
+
+  #Unistall APP
+  def delete_app(name)
+    app = apps.find {|istalled_app| istalled_app.name == name}
+    apps.delete(app) unless app.nil?
+  end
+
 end
 
 
 phone = Gadget.new("newuser002", "standardpassower001")
 puts phone.info
+
