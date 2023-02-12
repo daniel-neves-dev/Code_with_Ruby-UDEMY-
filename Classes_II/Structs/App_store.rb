e0ce7ad0e430cd144886a72c4b5bc47b4d15cd89
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-02-12 15:38:22 -0300

    feat: modeule AppStore

---
+++ Classes_II/Structs/App_store.rb
@@ -0,0 +1,14 @@
+module AppStore
+
+  App = Struct(:name, :developer, :version)
+
+  APPS = [
+    App.new(:Chat, :Facebook, :2.3)
+    App.new(:Twitter,:Twitter, :5.8)
+    App.new(:Weather, :Yahoo, :12.5)
+  ]
+
+  def self.find_app(name)
+    APPS.find{|app| app.name == name}
+  end
+end
