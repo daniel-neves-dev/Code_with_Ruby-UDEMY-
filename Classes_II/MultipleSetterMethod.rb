1c1ebeebc3d58a63835d151b48cb20a55cf24bb4
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-02-12 12:00:04 -0300

    feat: reset password

--- Classes_II/MultipleSetterMethod.rb
+++ Classes_II/MultipleSetterMethod.rb
@@ -9,15 +9,25 @@ class Gadget
     @production_number = automatic_production_number
   end
 
+  #Reseting password
+  def password = (new_password) 
+    @password = new_password if validade_password(new_password)
+  end
+
   def automatic_production_number
     digits = rand(100..999)
     alphabet = ("A".."Z").to_a.sample(2).join
     year = 2023
     "#{digits}#{alphabet}#{year}"
   end
-
+  #Gadget informations
   def info
     "User name: #{self.username} - Password: #{self.password} - Production number: #{self.production_number}"
+  end
+
+  #Reseting password
+  def validade_password(new_password) 
+    new_password.is_a(String) && new_password.length >= 6 && new_password =~ /\d/
   end
 
 end
